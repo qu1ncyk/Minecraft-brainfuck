fb71e1442827bae69a934db6841643ef9bbaf9ed
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2020-07-02 11:46:53 +0200

    rewrote cd.py, because StackOverflow-copied code is incompatible with GPLv3

--- behavior_packs/Brainfuck/functions/bf/create_print.py
+++ behavior_packs/Brainfuck/functions/bf/create_print.py
@@ -5,9 +5,13 @@ def make_command(num):
 	char=json.dumps(chr(num))[1:-1]
 	return "execute @a[tag=value,scores={value="+str(num)+"}] ~~~ tellraw @a {\"rawtext\": [{\"text\": \""+char+" ("+str(num)+")\"}]}"
 
-content="#Generated using create_print.py"
-for i in range(256):
-	content+="\n"+make_command(i)
-file=open("print.mcfunction", "w")
-file.write(content)
-file.close()
+def main():
+	content="#Generated using create_print.py"
+	for i in range(256):
+		content+="\n"+make_command(i)
+	file=open("print.mcfunction", "w")
+	file.write(content)
+	file.close()
+
+if __name__=="__main__":
+	main()
